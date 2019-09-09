2c4fec6daa4d42db6cf24b44497c2ea10fbe4b1c
MeesterMarcus <lorenzanamarcus@gmail.com>
2019-09-09 18:32:33 -0500

    more cars

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,5 +11,9 @@ INSERT INTO CARS
   (MAKE, MODEL, YEAR)
 VALUES
   ('Lexus', 'ES300', 2002),
+  ('Lexus', 'IS250', 2015),
+  ('Lexus', 'IS250', 2016),
   ('Toyota', 'Camry', 2015),
-  ('Ford', 'Mustang', 2017);
+  ('Toyota', 'Forerunner', 2019),
+  ('Ford', 'Mustang', 2017),
+  ('Ford', 'Mustang', 2018);
